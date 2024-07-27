a4bdecf4f4a593422e04dfedf2548d7e72e3a91b
Softlaptop <Softlaptop@DESKTOP-3CE5M2E>
2024-07-27 21:42:48 +0300

    part 4

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -56,6 +56,40 @@ namespace Demo
             //IFlyable movable2 = new Aroplane ();
             //movable2.Forward();
             #endregion
+
+            #region shallow copy and deep copy
+            //int[] arr1 = { 1, 2, 3 };
+            //int[] arr2 = new int[3];
+
+            //Console.WriteLine("HC OF ARR1 : " + arr1.GetHashCode());
+            //Console.WriteLine("HC OF ARR1 : " + arr2.GetHashCode());
+
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+
+            //arr2 = arr1;
+
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+
+            //arr2 [0] = 100;
+
+            //Console.WriteLine(arr2[0]);
+            //Console.WriteLine(arr2[0]);
+
+            //arr2 = (int[]) arr1.Clone();
+
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+
+            //arr2 [0] = 100;
+
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+            #endregion
+
+            #region 
+            #endregion
         }
     }
 }
